6d87a7e543b5f054c355f9810186486eb50e8f64
Oussama Tifara <oussamatifala@gmail.com>
2026-04-19 22:12:03 -0400

    Correction view_salle et ajout des boutons CRUD

--- views/view_salle.py
+++ views/view_salle.py
@@ -2,13 +2,14 @@ import customtkinter as ctk
 from services.services_salle import ServiceSalle
 from models.salle import Salle
 
+
 class ViewSalle(ctk.CTk):
 
     def __init__(self):
         super().__init__()
 
         self.title("Gestion des salles")
-        self.geometry("400x400")
+        self.geometry("400x500")
 
         self.service = ServiceSalle()
 
@@ -31,6 +32,7 @@ class ViewSalle(ctk.CTk):
 
         btn_show = ctk.CTkButton(self, text="Afficher", command=self.afficher_salles)
         btn_show.pack(pady=5)
+
         btn_update = ctk.CTkButton(self, text="Modifier", command=self.modifier_salle)
         btn_update.pack(pady=5)
 
@@ -39,59 +41,6 @@ class ViewSalle(ctk.CTk):
 
         btn_search = ctk.CTkButton(self, text="Rechercher", command=self.rechercher_salle)
         btn_search.pack(pady=5)
-
-    def ajouter_salle(self):
-        code = self.entry_code.get()
-        libelle = self.entry_libelle.get()
-        type_s = self.entry_type.get()
-
-        try:
-            capacite = int(self.entry_capacite.get())
-        except:
-            print("Capacité invalide")
-            return
-
-        s = Salle(code, libelle, type_s, capacite)
-        self.service.ajouter_salle(s)
-
-    def afficher_salles(self):
-        liste = self.service.recuperer_salles()
-        for s in liste:
-            s.afficher_infos()
-            print("------")
-import customtkinter as ctk
-from services.services_salle import ServiceSalle
-from models.salle import Salle
-
-class ViewSalle(ctk.CTk):
-
-    def __init__(self):
-        super().__init__()
-
-        self.title("Gestion des salles")
-        self.geometry("400x400")
-
-        self.service = ServiceSalle()
-
-        # Inputs
-        self.entry_code = ctk.CTkEntry(self, placeholder_text="Code")
-        self.entry_code.pack(pady=5)
-
-        self.entry_libelle = ctk.CTkEntry(self, placeholder_text="Libellé")
-        self.entry_libelle.pack(pady=5)
-
-        self.entry_type = ctk.CTkEntry(self, placeholder_text="Type")
-        self.entry_type.pack(pady=5)
-
-        self.entry_capacite = ctk.CTkEntry(self, placeholder_text="Capacité")
-        self.entry_capacite.pack(pady=5)
-
-        # Buttons
-        btn_add = ctk.CTkButton(self, text="Ajouter", command=self.ajouter_salle)
-        btn_add.pack(pady=5)
-
-        btn_show = ctk.CTkButton(self, text="Afficher", command=self.afficher_salles)
-        btn_show.pack(pady=5)
 
     def ajouter_salle(self):
         code = self.entry_code.get()
@@ -143,6 +92,6 @@ class ViewSalle(ctk.CTk):
             self.entry_type.insert(0, s.type)
 
             self.entry_capacite.delete(0, "end")
-            self.entry_capacite.insert(0, s.capacite)
+            self.entry_capacite.insert(0, str(s.capacite))
         else:
             print("Salle non trouvée")
