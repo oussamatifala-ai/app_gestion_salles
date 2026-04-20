9cb51eb5f3c41de2ff8bb93b72d3a92fe8144682
Oussama Tifara <oussamatifala@gmail.com>
2026-04-19 23:29:40 -0400

    Ajout commentaire dans DataSalle

--- Data/dao_salle.py
+++ Data/dao_salle.py
@@ -2,7 +2,7 @@ import json
 import mysql.connector
 from models.salle import Salle
 
-
+# Couche DAO pour l accès a la base de donnees
 class DataSalle:
     def get_connection(self):
         with open("Data/config.json", "r", encoding="utf-8") as f:
